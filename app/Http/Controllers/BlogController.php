02767d86ffed6e5d4829a20bf5df53f9b5fa32f4
kawada <vgnsl94@gmail.com>
2021-07-06 20:13:54 +0900

    s

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Blog;
+use App\Http\Requests\BlogRequest;
+
+use Illuminate\Support\Facades\DB;
 
 
 
@@ -40,6 +43,89 @@ class BlogController extends Controller
     public function showCreate() 
     {
         return view('blog.form');
+    }
+
+
+    public function exeStore(BlogRequest $request) 
+    {
+        // ブログのデータを受け取る
+        $inputs = $request->all();
+
+        DB::beginTransaction();
+        try {
+            // ブログを登録
+            Blog::create($inputs);
+            DB::commit();
+        } catch(\Throwable $e) {
+            DB::rollback();
+            abort(500);
+        }
+
+        return redirect(route('blogs'));
+    }
+
+    /**
+     * ブログ編集フォームを表示する
+     * @param int $id
+     * @return view
+     */
+    public function showEdit($id)
+    {
+        $blog = Blog::find($id);
+
+        if (is_null($blog)) {
+            return redirect(route('blogs'));
+        }
+
+        return view('blog.edit', ['blog' => $blog]);
+    }
+
+    /**
+     * ブログを更新する
+     * 
+     * @return view
+     */
+    public function exeUpdate(BlogRequest $request) 
+    {
+        // ブログのデータを受け取る
+        $inputs = $request->all();
+
+        DB::beginTransaction();
+        try {
+            // ブログを更新
+            $blog = Blog::find($inputs['id']);
+            $blog->fill([
+                'title' => $inputs['title'],
+                'content' => $inputs['content'],
+            ]);
+            $blog->save();
+            DB::commit();
+        } catch(\Throwable $e) {
+            DB::rollback();
+            abort(500);
+        }
+
+        return redirect(route('blogs'));
+    }
+    /**
+     * ブログ削除
+     * @param int $id
+     * @return view
+     */
+    public function exeDelete($id)
+    {
+        if (empty($id)) {
+            return redirect(route('blogs'));
+        }
+
+        try {
+            // ブログを削除
+            Blog::destroy($id);
+        } catch(\Throwable $e) {
+            abort(500);
+        }
+
+        return redirect(route('blogs'));
     }
 
 }
--- resources/views/blog/list.blade.php
+++ resources/views/blog/list.blade.php
@@ -1,31 +1,43 @@
 @extends('layout')
 @section('content')
 <div class="row">
-  <div class="col-md-8 col-md-offset-2">
-      <h2>ブログ記事一覧</h2>
-      <table class="table table-striped">
-          <tr>
-              <th>記事番号</th>
-              <th>日付</th>
-              <th>タイトル</th>
-              <th>内容</th>
-          </tr>
-          @foreach($blogs as $blog)
-          <tr>
-              <td>{{ $blog->id }}</td>
-              <td>{{ $blog->updated_at }}</td>
-
-              <td>
-                <a href="/blog/{{ $blog->id }}">
-                 {{ $blog->title }}
-                </a>
-              </td>
-
-              <td>{{ $blog->content }}</td>
-          </tr>
-          @endforeach
-      </table>
-
-  </div>
+    <div class="col-md-10 col-md-offset-2">
+        <h2>ブログ記事一覧</h2>
+        @if (session('err_msg'))
+            <p class="text-danger">
+                {{ session('err_msg') }}
+            </p>
+        @endif
+        <table class="table table-striped">
+            <tr>
+                <th>記事番号</th>
+                <th>タイトル</th>
+                <th>日付</th>
+                <th></th>
+                <th></th>
+            </tr>
+            @foreach($blogs as $blog)
+            <tr>
+                <td>{{ $blog->id  }}</td>
+                <td><a href="/blog/{{ $blog->id }}">{{ $blog->title  }}</a></td>
+                <td>{{ $blog->updated_at  }}</td>
+                <td><a class="btn btn-primary" href="{{ route('edit', $blog->id) }}">編集</a></td>
+                <form method="POST" action="{{ route('delete', $blog->id) }}" onSubmit="return checkDelete()">
+                @csrf
+                <td><button type="submit" class="btn btn-primary" onclick=>削除</button></td>
+                </form>
+            </tr>
+            @endforeach
+        </table>
+    </div>
 </div>
+<script>
+function checkDelete(){
+    if(window.confirm('削除してよろしいですか？')){
+        return true;
+    } else {
+        return false;
+    }
+}
+</script>
 @endsection
